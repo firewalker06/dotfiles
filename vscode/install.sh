c0dce2342acbf4ab51906e8b4add9cf1d0d12067
firewalker06 <didik.wicaksono@gmail.com>
2023-08-28 09:36:06 +0700

    Use brew bundler exclusively for MacAppStore and VSCode extensions management

--- vscode/install.sh
+++ vscode/install.sh
@@ -2,9 +2,6 @@
 
 # Install extensions
 if code -v > /dev/null; then
-  # Copying from https://github.com/Microsoft/vscode/issues/42994#issuecomment-365308504
-  cat "$(dirname $0)"/extensions.list | xargs -L1 code --install-extension
-
   # Symlink User folder
   rm -rf $HOME/Library/Application\ Support/Code/User
   ln -s $HOME/.dotfiles/vscode/User $HOME/Library/Application\ Support/Code/User
